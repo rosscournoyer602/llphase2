09ce086c4b9a58780569a1b73d49d12b96cbba15
Ross Cournoyer <rcoupvc@yahoo.com>
2019-07-21 13:42:53 +0800

    Team page, slider config

--- slider.js
+++ slider.js
@@ -1,29 +1,21 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
 $(document).ready(() => {
-  // eslint-disable-next-line no-undef
   $('.portrait-row').slick({
     centerMode: true,
-    centerPadding: '70px',
+    centerPadding: '40px',
     slidesToShow: 3,
     arrows: true,
     responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          arrows: false,
-          dots: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
       {
         breakpoint: 768,
         settings: {
           arrows: false,
           dots: true,
           centerMode: true,
-          centerPadding: '40px',
+          centerPadding: '20px',
           slidesToShow: 2
         }
       },
@@ -33,10 +25,57 @@ $(document).ready(() => {
           arrows: false,
           dots: true,
           centerMode: true,
-          centerPadding: '40px',
+          centerPadding: '20px',
           slidesToShow: 1
         }
       }
     ]
   });
+  $('.video-row').slick({
+    centerMode: true,
+    centerPadding: '20px',
+    slidesToShow: 1,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          dots: true,
+          centerMode: true,
+          centerPadding: '10px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
+
+$('.collapse').on('show.bs.collapse', function() {
+  console.log('collapge');
+  $('.video-row').resize();
+  $('.portrait-row').resize();
+  window.dispatchEvent(new Event('resize'));
+});
+
+$('.collapse').on('shown.bs.collapse', function() {
+  console.log('collapge');
+  $('.video-row').resize();
+  $('.portrait-row').resize();
+  window.dispatchEvent(new Event('resize'));
+});
+
+$('.collapse').on('hide.bs.collapse', function() {
+  console.log('collapge');
+  $('.video-row').resize();
+  $('.portrait-row').resize();
+  window.dispatchEvent(new Event('resize'));
+});
+
+$('.collapse').on('hidden.bs.collapse', function() {
+  console.log('collapge');
+  $('.video-row').resize();
+  $('.portrait-row').resize();
+  window.dispatchEvent(new Event('resize'));
 });
